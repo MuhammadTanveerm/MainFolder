5c73496a302309ca68ec137ca3c51b0c688b0bda
Muhammad <muhammadtanveer0242@gamil.com>
2023-10-24 02:41:44 +0500

    third branch chaange

--- functional.js
+++ functional.js
@@ -1,3 +1,11 @@
 
 
 // Now i am writing just commit about to learn how to add new new file in current repo in git
+
+
+const a = 3;
+const b=32;
+
+console(a+b);
+
+// this is for feacture11 we want of learn about 
